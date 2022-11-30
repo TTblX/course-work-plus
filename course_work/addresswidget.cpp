0e80c9553a293da0dd58360c101607dfa4a2df4c
TTblX <ivanluzhkov12@gmail.com>
2022-11-30 11:10:17 +0300

    reworked setupTable

--- course_work/addresswidget.cpp
+++ course_work/addresswidget.cpp
@@ -121,37 +121,61 @@ void AddressWidget::removeEntry()
 void AddressWidget::setupTabs()
 {
     using namespace Qt::StringLiterals;
-    const auto groups = { "ABC"_L1, "DEF"_L1, "GHI"_L1, "JKL"_L1, "MNO"_L1, "PQR"_L1,
-                          "STU"_L1, "VW"_L1, "XYZ"_L1 };
+//    const auto groups = { "ABC"_L1, "DEF"_L1, "GHI"_L1, "JKL"_L1, "MNO"_L1, "PQR"_L1,
+//                          "STU"_L1, "VW"_L1, "XYZ"_L1 };
 
-    for (QLatin1StringView str : groups) {
-        const auto regExp = QRegularExpression(QLatin1StringView("^[%1].*").arg(str),
-                                               QRegularExpression::CaseInsensitiveOption);
+//    for (QLatin1StringView str : groups) {
+//        const auto regExp = QRegularExpression(QLatin1StringView("^[%1].*").arg(str),
+//                                               QRegularExpression::CaseInsensitiveOption);
 
-        auto proxyModel = new QSortFilterProxyModel(this);
-        proxyModel->setSourceModel(table);
-        proxyModel->setFilterRegularExpression(regExp);
-        proxyModel->setFilterKeyColumn(0);
+//        auto proxyModel = new QSortFilterProxyModel(this);
+//        proxyModel->setSourceModel(table);
+//        proxyModel->setFilterRegularExpression(regExp);
+//        proxyModel->setFilterKeyColumn(0);
 
-        QTableView *tableView = new QTableView;
-        tableView->setModel(proxyModel);
-        tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
-        tableView->horizontalHeader()->setStretchLastSection(true);
-        tableView->verticalHeader()->hide();
-        tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
-        tableView->setSelectionMode(QAbstractItemView::SingleSelection);
-        tableView->setSortingEnabled(true);
+//        QTableView *tableView = new QTableView;
+//        tableView->setModel(proxyModel);
+//        tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+//        tableView->horizontalHeader()->setStretchLastSection(true);
+//        tableView->verticalHeader()->hide();
+//        tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+//        tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+//        tableView->setSortingEnabled(true);
 
-        connect(tableView->selectionModel(), &QItemSelectionModel::selectionChanged,
-                this, &AddressWidget::selectionChanged);
+//        connect(tableView->selectionModel(), &QItemSelectionModel::selectionChanged,
+//                this, &AddressWidget::selectionChanged);
 
-        connect(this, &QTabWidget::currentChanged, this, [this, tableView](int tabIndex) {
-            if (widget(tabIndex) == tableView)
-                emit selectionChanged(tableView->selectionModel()->selection());
-        });
+//        connect(this, &QTabWidget::currentChanged, this, [this, tableView](int tabIndex) {
+//            if (widget(tabIndex) == tableView)
+//                emit selectionChanged(tableView->selectionModel()->selection());
+//        });
 
-        addTab(tableView, str);
-    }
+//        addTab(tableView, str);
+//    }
+
+    auto proxyModel = new QSortFilterProxyModel(this);
+    proxyModel->setSourceModel(table);
+    proxyModel->setFilterKeyColumn(0);
+
+
+    QTableView *tableView  = new QTableView;
+    tableView->setModel(proxyModel);
+    tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    tableView->horizontalHeader()->setStretchLastSection(true);
+    tableView->verticalHeader()->hide();
+    tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+    tableView->setSortingEnabled(true);
+
+    connect(tableView->selectionModel(), &QItemSelectionModel::selectionChanged,
+             this, &AddressWidget::selectionChanged);
+
+   connect(this, &QTabWidget::currentChanged, this, [this, tableView](int tabIndex) {
+       if (widget(tabIndex) == tableView)
+           emit selectionChanged(tableView->selectionModel()->selection());
+   });
+
+   addTab(tableView, "ALL");
 }
 //! [1]
 
