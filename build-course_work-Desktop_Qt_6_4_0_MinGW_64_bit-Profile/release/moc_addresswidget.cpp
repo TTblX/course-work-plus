ed9a2af911f67c1021c989e7a7b3ee865b06c92b
TTblX <ivanluzhkov12@gmail.com>
2022-12-14 15:36:56 +0300

    вроде лист добавил, но эта хуйня через раз работаетЭ

---
+++ build-course_work-Desktop_Qt_6_4_0_MinGW_64_bit-Profile/release/moc_addresswidget.cpp
@@ -0,0 +1,222 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'addresswidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../course_work/addresswidget.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'addresswidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_AddressWidget_t {
+    uint offsetsAndSizes[28];
+    char stringdata0[14];
+    char stringdata1[17];
+    char stringdata2[1];
+    char stringdata3[15];
+    char stringdata4[9];
+    char stringdata5[19];
+    char stringdata6[9];
+    char stringdata7[5];
+    char stringdata8[8];
+    char stringdata9[6];
+    char stringdata10[12];
+    char stringdata11[10];
+    char stringdata12[10];
+    char stringdata13[12];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_AddressWidget_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_AddressWidget_t qt_meta_stringdata_AddressWidget = {
+    {
+        QT_MOC_LITERAL(0, 13),  // "AddressWidget"
+        QT_MOC_LITERAL(14, 16),  // "selectionChanged"
+        QT_MOC_LITERAL(31, 0),  // ""
+        QT_MOC_LITERAL(32, 14),  // "QItemSelection"
+        QT_MOC_LITERAL(47, 8),  // "selected"
+        QT_MOC_LITERAL(56, 18),  // "showAddEntryDialog"
+        QT_MOC_LITERAL(75, 8),  // "addEntry"
+        QT_MOC_LITERAL(84, 4),  // "name"
+        QT_MOC_LITERAL(89, 7),  // "address"
+        QT_MOC_LITERAL(97, 5),  // "email"
+        QT_MOC_LITERAL(103, 11),  // "picturePath"
+        QT_MOC_LITERAL(115, 9),  // "editEntry"
+        QT_MOC_LITERAL(125, 9),  // "findEntry"
+        QT_MOC_LITERAL(135, 11)   // "removeEntry"
+    },
+    "AddressWidget",
+    "selectionChanged",
+    "",
+    "QItemSelection",
+    "selected",
+    "showAddEntryDialog",
+    "addEntry",
+    "name",
+    "address",
+    "email",
+    "picturePath",
+    "editEntry",
+    "findEntry",
+    "removeEntry"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_AddressWidget[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   50,    2, 0x06,    1 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       5,    0,   53,    2, 0x0a,    3 /* Public */,
+       6,    4,   54,    2, 0x0a,    4 /* Public */,
+      11,    0,   63,    2, 0x0a,    9 /* Public */,
+      12,    0,   64,    2, 0x0a,   10 /* Public */,
+      13,    0,   65,    2, 0x0a,   11 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::QString,    7,    8,    9,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject AddressWidget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QTabWidget::staticMetaObject>(),
+    qt_meta_stringdata_AddressWidget.offsetsAndSizes,
+    qt_meta_data_AddressWidget,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_AddressWidget_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<AddressWidget, std::true_type>,
+        // method 'selectionChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QItemSelection &, std::false_type>,
+        // method 'showAddEntryDialog'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addEntry'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        // method 'editEntry'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'findEntry'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'removeEntry'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void AddressWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<AddressWidget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->selectionChanged((*reinterpret_cast< std::add_pointer_t<QItemSelection>>(_a[1]))); break;
+        case 1: _t->showAddEntryDialog(); break;
+        case 2: _t->addEntry((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[3])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[4]))); break;
+        case 3: _t->editEntry(); break;
+        case 4: _t->findEntry(); break;
+        case 5: _t->removeEntry(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QItemSelection >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (AddressWidget::*)(const QItemSelection & );
+            if (_t _q_method = &AddressWidget::selectionChanged; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *AddressWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *AddressWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_AddressWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QTabWidget::qt_metacast(_clname);
+}
+
+int AddressWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QTabWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void AddressWidget::selectionChanged(const QItemSelection & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
